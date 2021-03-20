6fc0a073f85de228833b14ff03b001b9039218c0
ElMekhtiev <61279962+ElMekhtiev@users.noreply.github.com>
2021-03-20 23:23:21 +0300

    Add files via upload

---
+++ markoviz-Copy1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+import pandas as pd
+# !pip install dwave-ocean-sdk
+
+
+# In[10]:
+
+
+d = pd.read_table('DATA_M_N=10_LENGTH=200.txt', delimiter = " ")
+d
+
